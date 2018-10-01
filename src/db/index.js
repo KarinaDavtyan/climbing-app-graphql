40c53e32ca3c47d821c9a54d81f9e6b0c6ed1182
KarinaDavtyan <k.davtyan27@gmail.com>
2018-10-01 17:51:44 +0200

    feat: creat route, delete and get by id

--- src/db/index.js
+++ src/db/index.js
@@ -1,23 +1,79 @@
  require('dotenv').config()
 const { idToString } = require('../helpers');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const url = process.env.MONGO_LOCAL_DB;
 const dbName = process.env.DB_NAME;
 
-const postRoute = async ({ data }) => {
+const createRoute = async ({ data }) => {
   let client;
   try {
     client = await MongoClient.connect(url, { useNewUrlParser: true });
     const db = client.db(dbName);
     const routes = db.collection('routes');
 
-    const { _id, title, location } = data;
+    const { name, difficulty, length_m, tags, style } = data;
 
-    const route = routes.insert({
-      title
+    const route = await routes.insertOne({
+      name,
+      difficulty,
+      length_m,
+      tags,
+      style
     })
-    return route;
+    if (route.insertedId) {
+      //FIX -  add spread operator
+      const res = Object.assign({_id: route.insertedId.toString()}, data)
+      return res;
+    } else {
+      return;
+    }
+  } catch (err) {
+    //eslint-disable-next-line
+    console.log(err.stack);
+  }
+  client.close();
+}
+
+const getRoute = async (data) => {
+  let client;
+  try {
+    client = await MongoClient.connect(url, { useNewUrlParser: true });
+    const db = client.db(dbName);
+    const routes = db.collection('routes');
+
+    const route = await routes.findOne({
+      _id: new ObjectId(data._id)
+    })
+    console.log(route);
+    if (route) {
+      route._id = route._id.toString();
+      return route
+    } else {
+      return;
+    }
+  } catch (err) {
+    //eslint-disable-next-line
+    console.log(err.stack);
+  }
+  client.close();
+}
+
+const deleteRoute = async (_id) => {
+  let client;
+  try {
+    client = await MongoClient.connect(url, { useNewUrlParser: true });
+    const db = client.db(dbName);
+    const routes = db.collection('routes');
+
+    const deletedRoute = await routes.deleteOne({
+      _id: new ObjectId(_id)
+    })
+    if (deletedRoute.deletedCount > 0) {
+      return _id
+    } else {
+      return;
+    }
   } catch (err) {
     //eslint-disable-next-line
     console.log(err.stack);
@@ -45,6 +101,7 @@ const getRoutes = async () => {
 
 
 module.exports = {
-  postRoute,
-  getRoutes
+  getRoute,
+  createRoute,
+  deleteRoute,
 }
--- src/graphQL/resolvers/index.js
+++ src/graphQL/resolvers/index.js
@@ -2,24 +2,62 @@ import db from '../../db';
 
 const resolvers = {
   Query: {
-    route:()=> {
-      return {
-        title: 'title'
-      }
-    },
-    routes: async () => {
-      const routes = await db.getRoutes();
-      return routes
+    route: async (root, args)=> {
+      const route = await db.getRoute({_id: args._id});
+      return route
     }
   },
   Mutation: {
-    postRoute: async (root, args) => {
-      const data = {
-        title: args.title
+    createRoute: async (root, args) => {
+      try {
+        const data = {
+          name: args.route.name,
+          difficulty: args.route.difficulty,
+          length_m: args.route.length_m,
+          tags: args.route.tags,
+          style: args.route.style
+        }
+        const route = await db.createRoute({ data });
+        if (route) {
+          const response = {
+            success: true,
+            message: `Route ${route._id} successfully added`
+          }
+          const result = Object.assign(
+            { route }, response
+          )
+          return result;
+        } else {
+          const response = {
+            success: false,
+            message: `Route ${data.name} NOT added`
+          }
+          return response;
+        }
+      } catch (e) {
+        console.log(e, "ERROR createRoute");
       }
-      await db.postRoute({ data })
-      return {
-        data
+    },
+    deleteRoute: async (root, args) => {
+      const { _id }= args;
+      let deletedID;
+      try {
+        deletedID = await db.deleteRoute(_id);
+        if (deletedID) {
+          return {
+            success: true,
+            message: `Route ${deletedID} successfully deleted`,
+            deleteRouteID: deletedID
+          }
+        } else {
+          return {
+            success: false,
+            message: `Route ${deletedID} NOT deleted`,
+            deleteRouteID: deletedID
+          }
+        }
+      } catch (e) {
+        console.log(e, "ERROR on deleteRoute");
       }
     }
   }
