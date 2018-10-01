6bbf02a6472b3f0f5faf39fa495357f87dd2baa4
KarinaDavtyan <k.davtyan27@gmail.com>
2018-10-01 21:41:33 +0200

    fix: add reference routes to associated climbing area on create, feat: add create climbing area

--- src/db/index.js
+++ src/db/index.js
@@ -8,23 +8,35 @@ const dbName = process.env.DB_NAME;
 const createRoute = async ({ data }) => {
   let client;
   try {
+
     client = await MongoClient.connect(url, { useNewUrlParser: true });
     const db = client.db(dbName);
     const routes = db.collection('routes');
+    const climbing_areas = db.collection('climbing_areas');
 
-    const { name, difficulty, length_m, tags, style } = data;
+    const { name, difficulty, length_m, tags, style, climbing_area_name } = data;
 
     const route = await routes.insertOne({
       name,
+      climbing_area_name,
       difficulty,
       length_m,
       tags,
       style
     })
+
     if (route.insertedId) {
-      //FIX -  add spread operator
-      const res = Object.assign({_id: route.insertedId.toString()}, data)
-      return res;
+      const targetClimbingArea =  await climbing_areas.updateOne(
+        { "name": climbing_area_name },
+        { $push: { routes: route.insertedId} }
+      )
+      if (targetClimbingArea.modifiedCount > 0) {
+        //FIX -  add spread operator
+        const res = Object.assign({_id: route.insertedId.toString()}, data)
+        return res;
+      } else {
+        console.log('route wasnt appended to climbing area');
+      }
     } else {
       return;
     }
@@ -99,9 +111,89 @@ const getRoutes = async () => {
 }
 
 
+const createClimbingArea = async ({ data }) => {
+  let client;
+  try {
+    client = await MongoClient.connect(url, { useNewUrlParser: true });
+    const db = client.db(dbName);
+    const climbing_areas = db.collection('climbing_areas');
+
+    const { name, country } = data;
+
+    const climbing_area = await climbing_areas.insertOne({
+      name,
+      country,
+      routes: [] //initialize array in order to push route's ID ref on create
+    })
+    if (climbing_area.insertedId) {
+      //FIX -  add spread operator
+      const res = Object.assign({_id: climbing_area.insertedId.toString()}, data)
+      return res;
+    } else {
+      return;
+    }
+  } catch (err) {
+    //eslint-disable-next-line
+    console.log(err.stack);
+  }
+  client.close();
+}
+
+const getClimbingArea = async (data) => {
+  let client;
+  try {
+    client = await MongoClient.connect(url, { useNewUrlParser: true });
+    const db = client.db(dbName);
+    const climbing_areas = db.collection('climbing_areas');
+
+    const climbing_area = await climbing_areas.findOne({
+      _id: new ObjectId(data._id)
+    })
+    console.log(climbing_area);
+    if (climbing_area) {
+      climbing_area._id = climbing_area._id.toString();
+      return climbing_area
+    } else {
+      return;
+    }
+  } catch (err) {
+    //eslint-disable-next-line
+    console.log(err.stack);
+  }
+  client.close();
+}
+
+const deleteClimbingArea = async (_id) => {
+  let client;
+  try {
+    client = await MongoClient.connect(url, { useNewUrlParser: true });
+    const db = client.db(dbName);
+    const climbing_areas = db.collection('climbing_areas');
+    const routes = db.collection('routes');
+
+
+    const deletedClimbingArea = await climbing_areas.deleteOne({
+      _id: new ObjectId(_id)
+    })
+
+    //FIX: delete all the included routes ???
+    if (deletedClimbingArea.deletedCount > 0) {
+      return _id
+    } else {
+      return;
+    }
+  } catch (err) {
+    //eslint-disable-next-line
+    console.log(err.stack);
+  }
+  client.close();
+}
+
 
 module.exports = {
   getRoute,
   createRoute,
   deleteRoute,
+  createClimbingArea,
+  getClimbingArea,
 }
--- src/graphQL/resolvers/index.js
+++ src/graphQL/resolvers/index.js
@@ -5,6 +5,10 @@ const resolvers = {
     route: async (root, args)=> {
       const route = await db.getRoute({_id: args._id});
       return route
+    },
+    climbing_area: async (root, args)=> {
+      const climbing_area = await db.getClimbingArea({_id: args._id});
+      return climbing_area
     }
   },
   Mutation: {
@@ -12,6 +16,7 @@ const resolvers = {
       try {
         const data = {
           name: args.route.name,
+          climbing_area_name: args.route.climbing_area_name,
           difficulty: args.route.difficulty,
           length_m: args.route.length_m,
           tags: args.route.tags,
@@ -58,6 +63,56 @@ const resolvers = {
         }
       } catch (e) {
         console.log(e, "ERROR on deleteRoute");
+      }
+    },
+    createClimbingArea: async (root, args) => {
+      try {
+        const data = {
+          name: args.climbing_area.name,
+          country: args.climbing_area.country,
+        }
+        const climbing_area = await db.createClimbingArea({ data });
+        if (climbing_area) {
+          const response = {
+            success: true,
+            message: `Climbing area ${climbing_area._id} successfully added`
+          }
+          const result = Object.assign(
+            { climbing_area }, response
+          )
+          return result;
+        } else {
+          const response = {
+            success: false,
+            message: `Climbing area ${data.name} NOT added`
+          }
+          return response;
+        }
+      } catch (e) {
+        console.log(e, "ERROR createClimbingArea");
+      }
+    },
+    //FIX: - Delete all routes associated
+    deleteClimbingArea: async (root, args) => {
+      const { _id }= args;
+      let deletedID;
+      try {
+        deletedID = await db.deleteClimbingArea(_id);
+        if (deletedID) {
+          return {
+            success: true,
+            message: `ClimbingArea ${deletedID} successfully deleted`,
+            deleteClimbingAreaID: deletedID
+          }
+        } else {
+          return {
+            success: false,
+            message: `ClimbingArea ${deletedID} NOT deleted`,
+            deleteClimbingAreaID: deletedID
+          }
+        }
+      } catch (e) {
+        console.log(e, "ERROR on deleteClimbingArea");
       }
     }
   }
