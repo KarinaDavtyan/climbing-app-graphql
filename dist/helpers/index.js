62843afff01b77df85ff94d263ac13a018a47c94
KarinaDavtyan <k.davtyan27@gmail.com>
2018-10-04 17:40:58 +0200

    Revert "Add dist folder"
    This reverts commit a7f0564108646b95b86f5c238ca5a6488497ab75.

--- dist/helpers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-const idToString = exports.idToString = doc => {
-  doc._id = doc._id.toString();
-  return doc;
-};
